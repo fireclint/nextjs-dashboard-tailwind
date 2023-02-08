6ebe9fc614ef46bf1bf4efc59206ac9139e741c7
milliorn <scottmilliorn@gmail.com>
2023-02-07 21:57:13 -0800

    fixed colors insufficient contrast ratio

--- components/RecentOrders.jsx
+++ components/RecentOrders.jsx
@@ -17,7 +17,7 @@ const RecentOrders = () => {
             </div>
             <div className='pl-4'>
               <p className='text-gray-800 font-bold'>${order.total}</p>
-              <p className='text-gray-400 text-sm'>{order.name.first}</p>
+              <p className='text-gray-600 text-sm'>{order.name.first}</p>
             </div>
             <p className='lg:flex md:hidden absolute right-6 text-sm'>{order.date}</p>
           </li>
